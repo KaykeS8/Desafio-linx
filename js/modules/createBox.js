60484684eca00e48a54fcf13bf59baba33cfb74a
kayke <kayke.py@gmail.com>
2022-01-29 22:01:48 -0300

    Arquivo javascript onde esta adicionando cada produto via js

---
+++ js/modules/createBox.js
@@ -0,0 +1,38 @@
+export default function initCreateProducts() { }
+
+
+const listProdutos = document.querySelector('.list-produtos')
+
+const boxList = createBox(produtos)
+function createBox() {
+    return produtos.map(item => {
+        const { name, image, oldPrice, price, description } = item
+        const { count, value } = item.installments
+        return (
+            `
+                <div class="produto-info">
+                        <div class="box"><img src="${image}" alt=""Produtos></div>
+                        <div class="box-flex">
+                            <h3 class="box-title">${name}</h3>
+                            <p class="descricao-produto">${description}.</p>
+                            <p class="priceOld">De: R$${oldPrice}</p>
+                            <p class="priceNew">Por: R$${price}</p>
+                            <p class="priceInTheCard">ou ${count}x de R$${value}</p>
+                            <button type="submit">Comprar</button>
+                        </div>
+                </div>
+                `
+        )
+    })
+}
+
+function insertInTheDom(dom) {
+    const contentHtml = dom.join('')
+    listProdutos.innerHTML = contentHtml
+}
+
+insertInTheDom(boxList)
+
+
+
+
