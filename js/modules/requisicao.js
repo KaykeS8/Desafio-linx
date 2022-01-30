43efc8faacd7328d3b67bb0ead852adff2b0b6f9
kayke <kayke.py@gmail.com>
2022-01-30 20:24:37 -0300

    Arquivo onde ocorre a requisição js

--- js/modules/requisicao.js
+++ js/modules/requisicao.js
@@ -1,12 +1,53 @@
 export default function initRequisicao() { }
 
-async function fetchProdutos() {
+export async function fetchProdutos(id) {
     try {
-        const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`
+        const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${id}`
         const response = await fetch(url)
         const productsDados = await response.json()
 
+
         const produtos = productsDados.products
+        const listProdutos = document.querySelector('.list-produtos')
+
+        const boxList = createBox(produtos)
+        function createBox() {
+            return produtos.map(item => {
+                const { name, image, oldPrice, price, description } = item
+                const { count, value } = item.installments
+                return (
+                    `
+                <div class="produto-info">
+                        <div class="box"><img src="${image}" alt=""Produtos></div>
+                        <div class="box-flex">
+                            <h3 class="box-title">${name}</h3>
+                            <p class="descricao-produto">${description}.</p>
+                            <p class="priceOld">De: R$${oldPrice}</p>
+                            <p class="priceNew">Por: R$${price}</p>
+                            <p class="priceInTheCard">ou ${count}x de R$${value}</p>
+                            <button type="submit">Comprar</button>
+                        </div>
+                </div>
+                `
+                )
+            })
+        }
+
+         function insertInTheDom(dom) {
+            const contentHtml = dom.join('')
+            listProdutos.innerHTML += contentHtml
+        }
+        insertInTheDom(boxList)
+
+        let count = 1
+        const adicionarProdutos = document.querySelector('[data-click="adc-produto"]')
+        adicionarProdutos.addEventListener('click', () => {
+            count++
+            fetchProdutos(count)
+            insertInTheDom()
+        })
+
+
 
     } catch (erro) {
         console.log(erro)
