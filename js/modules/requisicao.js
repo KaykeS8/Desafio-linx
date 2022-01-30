87b1ea6265e0ee98b724825bab5128c774d43fb1
kayke <kayke.py@gmail.com>
2022-01-29 22:02:32 -0300

    Arquivo javaScript onde ocorre a requisição via fetch da API

--- js/modules/requisicao.js
+++ js/modules/requisicao.js
@@ -1,51 +1,16 @@
 export default function initRequisicao() { }
 
-
-
 async function fetchProdutos() {
-    const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`
-    const response = await fetch(url)
-    const productsDados = await response.json()
+    try {
+        const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`
+        const response = await fetch(url)
+        const productsDados = await response.json()
 
-    const produtos = productsDados.products
-    const listProdutos = document.querySelector('.list-produtos')
-    
-    
-    const boxList = createBox(produtos)
-    function createBox() {
-        return produtos.map(item => {
-            const { name, image, oldPrice, price, description} = item
-            const {count, value} = item.installments
-            return (
-                `
-                <div class="produto-info">
-                        <div class="box"><img src="${image}" alt=""Produtos></div>
-                        <div class="box-flex">
-                            <h3 class="box-title">${name}</h3>
-                            <p class="descricao-produto">${description}.</p>
-                            <p class="priceOld">De: R$${oldPrice}</p>
-                            <p class="priceNew">Por: R$${price}</p>
-                            <p class="priceInTheCard">ou ${count}x de R$${value}</p>
-                            <button type="submit">Comprar</button>
-                        </div>
-                </div>
-                `
-            )
-        })
+        const produtos = productsDados.products
+
+    } catch (erro) {
+        console.log(erro)
     }
-
-    function insertInTheDom(dom) {
-        const contentHtml = dom.join('')
-        listProdutos.innerHTML = contentHtml
-    }
-
-    insertInTheDom(boxList)
-
-
-
-
-
 }
-
 
 fetchProdutos()
